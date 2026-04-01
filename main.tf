8ecdf489c1a6e089c6b7814d7c82c8d4ed033e80
Balakishore kanuparthi <kanuparthikishore@gmail.com>
2026-04-01 12:10:30 +0530

    Next day key value updated to dev_key1

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ resource "aws_instance" "public_instance" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.ssh.id]
-  key_name               = var.key_name
+  key_name               = var.key_name   #updated Key value to dev_key1
   tags = { Name = "${var.environment}-public-instance"
     Environment = var.environment
   }
